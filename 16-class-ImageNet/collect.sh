f4d2a1d0bbe85f7b03ad19d4aa8d777235dd0e07
Ben Evans <ben.d.evans@gmail.com>
2019-09-03 11:38:04 +0100

    Rename list_path and set out_dir

--- 16-class-ImageNet/collect.sh
+++ 16-class-ImageNet/collect.sh
@@ -1,8 +1,9 @@
 #!/bin/bash
 img_path="/storage/data/imagenet_2012"
-list_path="image_names"
+list_dir="image_names"
+out_dir="images"
 
-for file in $(ls $list_path/)
+for file in $(ls $list_dir/)
 do
   category=$(echo $file | cut -d "." -f 1)
   echo $category
@@ -12,6 +13,6 @@ do
   do
     folder=$(echo $image | cut -d "_" -f 1)
     #echo "Copying $img_path/$folder/$image into $category"
-    cp $img_path/$folder/$image $category/
-  done < "$list_path/$file"
+    cp $img_path/$folder/$image $out_dir/$category/
+  done < "$list_dir/$file"
 done
